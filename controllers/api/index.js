7a1fc96c75fe89749d36f850c04597e574f05697
Jakewilly14 <jakewilly14@gmail.com>
2024-01-17 00:41:29 -0600

    Rename Api endpoints

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-const userRoutes = require('./user');
-const categoriesRoutes = require('./categories');
-const transactionRoutes = require('./transaction');
+const userRoutes = require('./userRoutes');
+const categoriesRoutes = require('./categoryRoutes');
+const transactionRoutes = require('./transactionRoutes');
 
 router.use('/user', userRoutes);
 router.use('/categories', categoriesRoutes);
